802f8bfffefa21ef36115a2ef08a497e6cef4866
AEttinger <andreas.ettinger@ucsf.edu>
2015-02-07 21:45:38 -0800

    getting late commit

--- post2/post2/settings.py
+++ post2/post2/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')] # added 9:40 pm
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
--- post2/submit/views.py
+++ post2/submit/views.py
@@ -1,9 +1,46 @@
 from django.shortcuts import render
 
-# AEttinger 7.2.15 - added to view http content.
+# AEttinger 7.2.15 6:20 pm - add to render database context
+from submit.models import recipient
+
+
+# AEttinger 7.2.15 - added to view http content. 6:20 pm commented out
 from django.http import HttpResponse
+
+from django.template import loader, Context
 
 # Create your views here.
 
+# def index(request):
+# 	return HttpResponse("Hello world!")
+
 def index(request):
-	return HttpResponse("Hello world!")
+	last_recipient = recipient.objects.all()
+	context = {'last_recipient:' : last_recipient}
+
+	for r in last_recipient:
+		print(r.recipient_name)
+
+	print(type(last_recipient))
+
+	return render(request, 'submit/index.html', context)
+
+
+# for testing:
+def hello(request):
+	return HttpResponse("Hello World!")
+
+def view_data(request):
+	l = recipient.objects.all()
+	context = {'ID:' : l.id,
+				'name': l.name,
+				'street': l.recipient_street_text,
+				'street_nr': l.recipient_street_nr,
+				'city': l.recipient_city_text,
+				'zip': l.recipient_city_zip,
+				'country': l.recipient_country_text
+				}
+	for k in l:
+		print(k.name)
+	
+	return render(request, 'submit/index.html', context)
