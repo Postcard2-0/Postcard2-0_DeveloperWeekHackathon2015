3f3f8821c076e46f6c58f0b20480c6e93bbdd4a6
AEttinger <andreas.ettinger@ucsf.edu>
2015-02-07 16:27:21 -0800

    SQLlite generated

--- post2/submit/models.py
+++ post2/submit/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+# AEttinger 7.2.15 - I created two classes for the database, one for sender and one for recipient.
+
+# Recipient class:
+class recipient(models.Model):
+	recipient_name = models.CharField(max_length=200)
+	recipient_street_text = models.CharField(max_length=200)
+	recipient_street_nr = models.CharField(max_length=50)
+	recipient_city_text = models.CharField(max_length=200)
+	recipient_city_zip = models.CharField(max_length=50)
+	recipient_country_text = models.CharField(max_length=200)
+
+# Sender class:
+class sender(models.Model):
+	sender_name = models.CharField(max_length=200)
+	sender_memo = models.CharField(max_length=500)
+	sender_photo = models.ImageField(upload_to='photos')
+	sender_movie = models.URLField(max_length=200)
+
